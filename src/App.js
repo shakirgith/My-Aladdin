b0fcc20fdb92a0dd6ca165ebdc2815ec66e246da
shakirgith <86280465+shakirgith@users.noreply.github.com>
2021-09-30 13:05:56 +0530

    start
    ala

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './MyComponents/Layout/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './MyComponents/Pages';
+import About from './MyComponents/Pages/About';
+import Contact from './MyComponents/Pages/Contact';
+import Footer from './MyComponents/Layout/Footer';
+
+
 
 function App() {
   return (
-      <>
-      </ >
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+      <Footer />
+    </Router>
+    
   );
 }
 
 export default App;
+
+
+ 
---
+++ src/assets/js/main.js
@@ -0,0 +1,31 @@
+(function() {
+  "use strict";
+
+    // Preloader
+    $(window).on('load', function() {
+      if ($('#preloader').length) {
+        $('#preloader').delay(100).fadeOut('slow', function() {
+          $(this).remove();
+        });
+      }
+    });
+  
+
+
+
+// start resend code timer JSS
+var timeLeft = 30;
+var elem = document.getElementById('timer');
+var timerId = setInterval(countdown, 1000);
+
+function countdown() {
+    if (timeLeft == -1) {
+        clearTimeout(timerId);
+        doSomething();
+    } else {
+        elem.innerHTML = timeLeft + '';
+        timeLeft--;
+    }
+}
+
+  
