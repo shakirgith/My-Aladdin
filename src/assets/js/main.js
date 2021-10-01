96275ee6f19a645f3fe83b5a8e2458219ccd7fa4
shakirgith <86280465+shakirgith@users.noreply.github.com>
2021-10-01 15:12:30 +0530

    done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,31 +1,31 @@
-(function() {
-  "use strict";
+// (function() {
+//   "use strict";
 
-    // Preloader
-    $(window).on('load', function() {
-      if ($('#preloader').length) {
-        $('#preloader').delay(100).fadeOut('slow', function() {
-          $(this).remove();
-        });
-      }
-    });
+//     // Preloader
+//     $(window).on('load', function() {
+//       if ($('#preloader').length) {
+//         $('#preloader').delay(100).fadeOut('slow', function() {
+//           $(this).remove();
+//         });
+//       }
+//     });
   
 
 
 
 // start resend code timer JSS
-var timeLeft = 30;
-var elem = document.getElementById('timer');
-var timerId = setInterval(countdown, 1000);
+// var timeLeft = 30;
+// var elem = document.getElementById('timer');
+// var timerId = setInterval(countdown, 1000);
 
-function countdown() {
-    if (timeLeft == -1) {
-        clearTimeout(timerId);
-        doSomething();
-    } else {
-        elem.innerHTML = timeLeft + '';
-        timeLeft--;
-    }
-}
+// function countdown() {
+//     if (timeLeft == -1) {
+//         clearTimeout(timerId);
+//         doSomething();
+//     } else {
+//         elem.innerHTML = timeLeft + '';
+//         timeLeft--;
+//     }
+// }
 
   
