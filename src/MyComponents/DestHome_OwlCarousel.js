124d4eb352706fdfbc601a8abe922070c1709535
shakirgith <86280465+shakirgith@users.noreply.github.com>
2021-10-11 18:35:50 +0530

    11 oct

--- src/MyComponents/DestHome_OwlCarousel.js
+++ src/MyComponents/DestHome_OwlCarousel.js
@@ -1,48 +1,216 @@
-import React from 'react';  
-import OwlCarousel from 'react-owl-carousel';  
+//Owl Carousel Libraries and Module
+import React from 'react';
+import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';  
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import desImage from '../assets/images/destination1.jpeg';
+ 
+
+//Owl Carousel Settings
+const options = {
+  margin: 30,
+  responsiveClass: true,
+  nav: false,
+  dot: true,
+  autoplay: true,
+  navText: ["<i className='icofont-simple-left'></i>", "<i className='icofont-simple-right'></i>"],
+  smartSpeed: 1000,
+  responsive: {
+      0: {
+          items: 1,
+      },
+      400: {
+          items: 1,
+      },
+      600: {
+          items: 2,
+      },
+      768: {
+          items: 3,
+      },
+      1000: {
+        items: 4,
+    }
+     
+  }
+};
+class HomeDestOwlCarousel extends React.Component {
+  render() {
+   
+    return (
+<div>
+<OwlCarousel className="slider-items owl-carousel" {...options}>
+                    <div className="item">
+                        <div className="item-single mb-30">
+                            <div className="image">
+                               <img className="img-fluid" alt="Destination" src={desImage} />
+
+                            </div>
+                            <div className="content">
+                                <span className="location"><i class="bx bx-map"></i>Navi, Mumbai</span>
+                                <h3><a href="/">Royal Palace Hotel</a></h3>
+                                <div className="review">
+                                <i class="icofont-nerd-smile"></i>
+                                  <span>8.5</span>
+                                  <span>Superb</span>
+                                </div>
+                            <p>
+                            A wonderful little cottage right on the seashore - perfect for exploring.
+                            </p>
+                            <hr />
+                            <ul className="list">
+                            <li><i class="icofont-clock-time"></i>3 Days</li>
+                            <li><i class="icofont-ui-user-group"></i>160+</li>
+                            <li>$500</li>
+                            </ul>
+                         </div>
+                      <div className="spacer"></div>
+                 </div>
+              </div> 
+
+              <div className="item">
+                        <div className="item-single mb-30">
+                            <div className="image">
+                               <img className="img-fluid" alt="Destination" src={desImage} />
+
+                            </div>
+                            <div className="content">
+                                <span className="location"><i class="bx bx-map"></i>Navi, Mumbai</span>
+                                <h3><a href="/">Royal Palace Hotel</a></h3>
+                                <div className="review">
+                                <i class="icofont-nerd-smile"></i>
+                                  <span>8.5</span>
+                                  <span>Superb</span>
+                                </div>
+                            <p>
+                            A wonderful little cottage right on the seashore - perfect for exploring.
+                            </p>
+                            <hr />
+                            <ul className="list">
+                            <li><i class="icofont-clock-time"></i>3 Days</li>
+                            <li><i class="icofont-ui-user-group"></i>160+</li>
+                            <li>$500</li>
+                            </ul>
+                         </div>
+                      <div className="spacer"></div>
+                 </div>
+              </div> 
 
 
+              <div className="item">
+                        <div className="item-single mb-30">
+                            <div className="image">
+                               <img className="img-fluid" alt="Destination" src={desImage} />
 
-function HomeDestOwlCarousel () {
-  return (
-    <div>
-        <OwlCarousel items={3}  
-          className="owl-theme"  
-          loop  
-          nav  
-          margin={8} autoplay ={true}>  
-           <div ><img className="img-fluid" src= {'../assets/images/destination1.jpeg'} alt=""/></div> 
-           <div ><img  className="img-fluid" src= {'../assets/images/destination1.jpeg'} alt=""/></div>
-           <div ><img  className="img-fluid" src= {'../assets/images/destination1.jpeg'} alt=""/></div>
-           <div ><img  className="img-fluid" src= {'../assets/images/destination1.jpeg'} alt=""/></div>
-           <div ><img  className="img-fluid" src= {'../assets/images/destination1.jpeg'} alt=""/></div> 
-           <div ><img  className="img-fluid" src= {'../assets/images/destination1.jpeg'} alt=""/></div>
-          
-      </OwlCarousel>  
-    </div>
-  )
+                            </div>
+                            <div className="content">
+                                <span className="location"><i class="bx bx-map"></i>Navi, Mumbai</span>
+                                <h3><a href="/">Royal Palace Hotel</a></h3>
+                                <div className="review">
+                                <i class="icofont-nerd-smile"></i>
+                                  <span>8.5</span>
+                                  <span>Superb</span>
+                                </div>
+                            <p>
+                            A wonderful little cottage right on the seashore - perfect for exploring.
+                            </p>
+                            <hr />
+                            <ul className="list">
+                            <li><i class="icofont-clock-time"></i>3 Days</li>
+                            <li><i class="icofont-ui-user-group"></i>160+</li>
+                            <li>$500</li>
+                            </ul>
+                         </div>
+                      <div className="spacer"></div>
+                 </div>
+              </div> 
+
+              <div className="item">
+                        <div className="item-single mb-30">
+                            <div className="image">
+                               <img className="img-fluid" alt="Destination" src={desImage} />
+
+                            </div>
+                            <div className="content">
+                                <span className="location"><i class="bx bx-map"></i>Navi, Mumbai</span>
+                                <h3><a href="/">Royal Palace Hotel</a></h3>
+                                <div className="review">
+                                <i class="icofont-nerd-smile"></i>
+                                  <span>8.5</span>
+                                  <span>Superb</span>
+                                </div>
+                            <p>
+                            A wonderful little cottage right on the seashore - perfect for exploring.
+                            </p>
+                            <hr />
+                            <ul className="list">
+                            <li><i class="icofont-clock-time"></i>3 Days</li>
+                            <li><i class="icofont-ui-user-group"></i>160+</li>
+                            <li>$500</li>
+                            </ul>
+                         </div>
+                      <div className="spacer"></div>
+                 </div>
+              </div> 
+
+              <div className="item">
+                        <div className="item-single mb-30">
+                            <div className="image">
+                               <img className="img-fluid" alt="Destination" src={desImage} />
+
+                            </div>
+                            <div className="content">
+                                <span className="location"><i class="bx bx-map"></i>Navi, Mumbai</span>
+                                <h3><a href="/">Royal Palace Hotel</a></h3>
+                                <div className="review">
+                                <i class="icofont-nerd-smile"></i>
+                                  <span>8.5</span>
+                                  <span>Superb</span>
+                                </div>
+                            <p>
+                            A wonderful little cottage right on the seashore - perfect for exploring.
+                            </p>
+                            <hr />
+                            <ul className="list">
+                            <li><i class="icofont-clock-time"></i>3 Days</li>
+                            <li><i class="icofont-ui-user-group"></i>160+</li>
+                            <li>$500</li>
+                            </ul>
+                         </div>
+                      <div className="spacer"></div>
+                 </div>
+              </div> 
+
+              <div className="item">
+                        <div className="item-single mb-30">
+                            <div className="image">
+                               <img className="img-fluid" alt="Destination" src={desImage} />
+
+                            </div>
+                            <div className="content">
+                                <span className="location"><i class="bx bx-map"></i>Navi, Mumbai</span>
+                                <h3><a href="/">Royal Palace Hotel</a></h3>
+                                <div className="review">
+                                <i class="icofont-nerd-smile"></i>
+                                  <span>8.5</span>
+                                  <span>Superb</span>
+                                </div>
+                            <p>
+                            A wonderful little cottage right on the seashore - perfect for exploring.
+                            </p>
+                            <hr />
+                            <ul className="list">
+                            <li><i class="icofont-clock-time"></i>3 Days</li>
+                            <li><i class="icofont-ui-user-group"></i>160+</li>
+                            <li>$500</li>
+                            </ul>
+                         </div>
+                      <div className="spacer"></div>
+                 </div>
+              </div> 
+                  </OwlCarousel>
+</div>
+)
+};
 }
-
-
-export default HomeDestOwlCarousel
-
-
-// export class HomeDestOwlCarousel extends Component {  
-
-//         render()  
-//         {  
-//           return (  
-
-//        <div class='container-fluid' > 
-       
-        
-//       </div>  
-
-//           )  
-//         }  
-//       }  
-        
-  
-// export default HomeDestOwlCarousel
+export default HomeDestOwlCarousel;
--- src/MyComponents/Layout/Footer.js
+++ src/MyComponents/Layout/Footer.js
@@ -5,12 +5,85 @@ import React from 'react';
   
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer main-footer">
+		<div class="footer-inner-wrap container">
+			<div class="row">	
+				<div class="footer-widget-1 col-lg-4 col-md-6">
+					<div id="travcontactwidget-2" class="contact-box small-box widget_travcontactwidget"><h2 class="widgettitle">Need Travelo Help?</h2><p class="description">We would be more than happy to help you. Our team advisor are 24/7 at your service to help you.</p><div class="contact-info"><span class="phone-num"><i class="fas fa-phone-volume"></i> 1-800-123-HELLO</span><span class="email-address">hello@travelo.com</span></div></div><div id="social_links-widget-2" class="small-box social_links">
+			<div class="social-icons square-shape-icon">
+				
+									<a title="twitter" class="twitter-link" href="https://www.twitter.com/soaptheme" target="_blank" data-toggle="tooltip"><i class="fab fa-twitter"></i></a>
+				
+				
+									<a title="facebook" class="facebook-link" href="https://www.facebook.com/soaptheme" target="_blank" data-toggle="tooltip"><i class="fab fa-facebook-f"></i></a>
+				
+								<a title="linkedin" class="linkedin-link" href="https://www.linkedin.com/soaptheme" target="_blank" data-toggle="tooltip"><i class="fab fa-linkedin-in"></i></a>
+				
+				
+									<a title="pinterest" class="pinterest-link" href="https://www.pinterest.com/soaptheme" target="_blank" data-toggle="tooltip"><i class="fab fa-pinterest-p"></i></a>
+				
+				
+				
+									<a title="instagram" class="instagram-link" href="https://www.instagram.com/?hl=en" target="_blank" data-toggle="tooltip"><i class="fab fa-instagram"></i></a>
+				
+				
+				
+				
+				
+				
+				
+				
+			</div>
+			</div>				</div>
+
+				<div class="footer-widget-2 col-lg-2 col-md-3">
+					<div id="nav_menu-2" class="small-box widget_nav_menu"><h2 class="widgettitle">Company</h2><div class="menu-footer-menu-1-container"><ul id="menu-footer-menu-1" class="menu"><li id="menu-item-139" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-139"><a href="https://travelo.c-themes.com/about-us/">About</a></li>
+<li id="menu-item-141" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-141"><a href="#">Careers</a></li>
+<li id="menu-item-142" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-142"><a href="#">Mobile</a></li>
+<li id="menu-item-140" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-140"><a href="https://travelo.c-themes.com/blog/">Blog</a></li>
+<li id="menu-item-143" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-143"><a href="#">How We Work</a></li>
+<li id="menu-item-144" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-144"><a href="#">Press</a></li>
+<li id="menu-item-145" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-145"><a href="#">Investor Relations</a></li>
+</ul></div></div>				</div>
+
+				<div class="footer-widget-3 col-lg-2 col-md-3">
+					<div id="nav_menu-3" class="small-box widget_nav_menu"><h2 class="widgettitle">Useful</h2><div class="menu-footer-menu-2-container"><ul id="menu-footer-menu-2" class="menu"><li id="menu-item-146" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-146"><a href="https://travelo.c-themes.com/faqs/">Help/FAQs</a></li>
+<li id="menu-item-147" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-147"><a href="#">Legal</a></li>
+<li id="menu-item-148" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-148"><a href="#">Affiliates</a></li>
+<li id="menu-item-149" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-149"><a href="#">Hotel Owners</a></li>
+<li id="menu-item-150" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-150"><a href="#">Privacy Policy</a></li>
+<li id="menu-item-151" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-151"><a href="#">Sitemap</a></li>
+</ul></div></div>				</div>
+
+				<div class="footer-widget-4 col-lg-2 col-md-3">
+					<div id="nav_menu-4" class="small-box widget_nav_menu"><h2 class="widgettitle">Explore</h2><div class="menu-footer-menu-3-container"><ul id="menu-footer-menu-3" class="menu"><li id="menu-item-152" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-152"><a href="#">Read Reviews</a></li>
+<li id="menu-item-153" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-153"><a href="#">Traveler’s Choice</a></li>
+<li id="menu-item-154" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-154"><a href="#">Popular Cities</a></li>
+<li id="menu-item-155" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-155"><a href="#">Stories</a></li>
+<li id="menu-item-156" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-156"><a href="#">Travelo Store</a></li>
+<li id="menu-item-157" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-157"><a href="#">Help Center</a></li>
+</ul></div></div>				</div>
+
+				<div class="footer-widget-5 col-lg-2 col-md-3">
+					<div id="travsettingswidget-2" class="contact-box small-box widget_travsettingswidget"><h2 class="widgettitle">Settings</h2>				<div class="multi-currency dropdown">
+					<a class="currency-link dropdown-toggle" href="#" id="multi-curr-Dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">usd</a>
+					
+					<div class="dropdown-menu animate fadeIn" aria-labelledby="multi-curr-Dropdown" x-placement="bottom-start">
+						<a href="/?selected_currency=eur" class="dropdown-item" title="Euro">EUR</a><a href="/?selected_currency=usd" class="dropdown-item" title="United States Dollar">USD</a><a href="/?selected_currency=cad" class="dropdown-item" title="Canada Dollar">CAD</a><a href="/?selected_currency=cny" class="dropdown-item" title="China Yuan Renminbi">CNY</a>					</div>
+				</div>
+			</div>				</div>
+			</div>
+		</div>
+
+{/* 
       <div className="container-fluid">
         <div className="copyright">
         <p>Copyright &copy; My Apps 2021. All Right Reserved.</p>
         </div>
-      </div>
+      </div> */}
+      <div className="copyright">
+        <p>Copyright &copy; My Apps 2021. All Right Reserved.</p>
+        </div>
     </footer>
   );
 };
