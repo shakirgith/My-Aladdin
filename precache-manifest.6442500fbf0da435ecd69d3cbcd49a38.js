fb11f7afcdbbbdb918fb8657a5edb172fc3e8e88
shakirgith <86280465+shakirgith@users.noreply.github.com>
2021-11-19 16:58:11 +0530

    Updates

---
+++ precache-manifest.6442500fbf0da435ecd69d3cbcd49a38.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1214483df53ad982406e2f059dca7154",
+    "url": "/my-aladdin/index.html"
+  },
+  {
+    "revision": "086fa5a312a2648c9a04",
+    "url": "/my-aladdin/static/css/2.ea24616f.chunk.css"
+  },
+  {
+    "revision": "5573e0a1a798aae9fef6",
+    "url": "/my-aladdin/static/css/main.837b91c3.chunk.css"
+  },
+  {
+    "revision": "086fa5a312a2648c9a04",
+    "url": "/my-aladdin/static/js/2.a4bc40c9.chunk.js"
+  },
+  {
+    "revision": "a6f3c001137bb93a804657dd621b71b9",
+    "url": "/my-aladdin/static/js/2.a4bc40c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc00ed7bf3ecbbc28a89",
+    "url": "/my-aladdin/static/js/3.1af14248.chunk.js"
+  },
+  {
+    "revision": "5573e0a1a798aae9fef6",
+    "url": "/my-aladdin/static/js/main.53d3d5e0.chunk.js"
+  },
+  {
+    "revision": "78edf786f132c10291b9",
+    "url": "/my-aladdin/static/js/runtime-main.29f7f58f.js"
+  },
+  {
+    "revision": "c7d039dc0f821cf726adf37137afe2ee",
+    "url": "/my-aladdin/static/media/citysiloutt.c7d039dc.svg"
+  },
+  {
+    "revision": "20013c0727cd0b66d189c03d1d81bdbf",
+    "url": "/my-aladdin/static/media/destination1.20013c07.jpeg"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/my-aladdin/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/my-aladdin/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "553ce532e6d17a471df462b54aa2ec49",
+    "url": "/my-aladdin/static/media/logo-clipart.553ce532.png"
+  },
+  {
+    "revision": "56efb5d5259481bd27a06816bf45bcf1",
+    "url": "/my-aladdin/static/media/secure-payment-page-960x300-1.56efb5d5.gif"
+  },
+  {
+    "revision": "1904fdd032f9ccfbb8391de0fd99212c",
+    "url": "/my-aladdin/static/media/xbg_1.jpg.pagespeed.ic.GQT90DL5zP.1904fdd0.webp"
+  }
+]);
